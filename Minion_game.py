1341b03efb71a7deccb959867b591b351448bbf0
akrepkov <115089321+akrepkov@users.noreply.github.com>
2024-08-11 16:01:57 +0100

    Create Minion_game.py

---
+++ Minion_game.py
@@ -0,0 +1,39 @@
+Kevin and Stuart want to play the 'The Minion Game'.
+
+# Game Rules
+
+# Both players are given the same string, .
+# Both players have to make substrings using the letters of the string .
+# Stuart has to make words starting with consonants.
+# Kevin has to make words starting with vowels.
+
+# Scoring
+# A player gets +1 point for each occurrence of the substring in the string 
+
+def minion_game(string):
+# Count of non-empty substrings is n*(n+1)/2
+# Number of substrings of length one is n 
+# Number of substrings of length two is n-1 
+# Number of substrings of length k is n-k+1 where 1 <= k <= n
+# Total number of substrings of all lengths from 1 to n = 
+# n + (n-1) + (n-2) + (n-3) + … 2 + 1 
+# = n * (n + 1)/2
+    vowels = "AEIOU"
+    stuart_score = 0
+    kevin_score = 0
+    length = len(string)
+    for i in range(length):
+        if string[i] in vowels: #check if it start with vowel
+            kevin_score += (length - i) #n + (n-1) + (n-2) + (n-3)
+        else:
+            stuart_score += (length - i) #n + (n-1) + (n-2) + (n-3)
+    if kevin_score > stuart_score:
+        print(f"Kevin {kevin_score}")
+    elif stuart_score > kevin_score:
+        print(f"Stuart {stuart_score}")
+    else:
+        print("Draw")
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
